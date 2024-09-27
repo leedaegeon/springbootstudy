6aa5c310139b341a015feb3a44a91073be14419d
leedaegeon <ldg031@pusan.ac.kr>
2024-09-27 22:15:26 +0900

    springboot: add spring bean

--- src/main/java/toby/hellosboot/HellosbootApplication.java
+++ src/main/java/toby/hellosboot/HellosbootApplication.java
@@ -4,6 +4,7 @@ package toby.hellosboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -16,24 +17,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HellosbootApplication {
-
     public static void main(String[] args) {
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
             servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                     // 인증, 보안, 다국어등에 필요한 공통 기능을 이 프론트 컨트롤러가 처리
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        // 200번 상태 코드는 생략 가능
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().print(ret);
-                    } else if (req.getRequestURI().equals("/user")) {
-                        //
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
